617f071e4df18dc4084fa1b3319d2b2fe2a51819
Niceplugin <niceplugin@gmail.com>
2022-12-01 18:22:44 +0900

    Update method.js

--- imports/api/method.js
+++ imports/api/method.js
@@ -8,6 +8,7 @@ Meteor.methods({
       throw new Meteor.Error("not Authorized!!!")
     }
     const userId = Meteor.userId()
+    // todo - 로그인한 유저이기만 하면 누구든지 이 메서드 콜 할 경우 다 지울수 있는 거네요?
     Attendance.remove({
       // user_id:userId
     })
