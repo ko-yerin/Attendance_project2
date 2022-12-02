680353ca02ca1705d7ddbe9f2bec26cb2d8a7bf7
anhyoseung <ace.aaiden@gmail.com>
2022-12-02 17:00:00 +0900

    aiden

--- client/main.js
+++ client/main.js
@@ -1,4 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './main.html'
 import '/imports/client/routes'
+// import * as bootstrap from 'bootstrap'
+// import 'bootstrap'
+
+// window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
+
+//🚨죄송합니다 css, sass컴파일러 설정이 필요한데 디렉토리가 헷갈려서 제대로 못한거같스빈다..
---
+++ imports/utils/modal.js
@@ -0,0 +1,5 @@
+// <template name="moda">
+//   <div className="alert alert-dark" role="alert">
+//     A simple dark alert—check it out!
+//   </div>
+// </template>
